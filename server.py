81a7c13cfed320fd0a07841a38972ed01e3a0fc3
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-27 17:36:21 -0700

    add success app route

--- server.py
+++ server.py
@@ -10,6 +10,13 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 def hello_world():
     return 'Hello World!'  # Return the string 'Hello World!' as a response
 
+# add an app route success
+@app.route('/success')
+def success():
+    return "success"
+
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
+
+
 
