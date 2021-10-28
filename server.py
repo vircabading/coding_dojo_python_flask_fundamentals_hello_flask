bb97b3c09c10fcdbf5f8cb8305c3a121aae47eed
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-27 17:40:38 -0700

    add an app route with a variable name

--- server.py
+++ server.py
@@ -5,15 +5,24 @@
 
 from flask import Flask  # Import Flask to allow us to create our app
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
 
+# **** Default App Route **********************************************
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
 def hello_world():
     return 'Hello World!'  # Return the string 'Hello World!' as a response
 
+# **** Success App Route **********************************************
 # add an app route success
 @app.route('/success')
 def success():
     return "success"
+
+# **** add an app route with a variable ******************************* 
+@app.route('/hello/<name>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+def hello(name):
+    print(name)
+    return "Hello, " + name
+
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
